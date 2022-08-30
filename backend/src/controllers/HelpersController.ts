9c5dc563807241f6f637823ed3a380506eede626
Felipe Blanco <feeh.blanco@gmail.com>
2022-08-30 17:14:30 -0300

    cenário de cadastro de food trucks

--- backend/src/controllers/HelpersController.ts
+++ backend/src/controllers/HelpersController.ts
@@ -11,4 +11,5 @@ export default {
 
         return res.status(204).end()
     }
+    
 }
---
+++ tests/cypress/e2e/create.cy.js
@@ -0,0 +1,32 @@
+import mapPage from "../support/pages/Map";
+import createPage from '../support/pages/Create';
+describe('Recomendação', () => {
+
+    it('deve recomendar um food truck', () => {
+
+        const user = {
+            name: 'Benson',
+            instagram: '@benson',
+            password: 'pwd123'
+        }
+
+        const foodtruck = {
+            latitude: '-23.584548837854058',
+            longitude: '-46.674446913517876',
+            name: 'Tienda Del Chavo',
+            details: 'O melhor lugar para tomar o suco de limão que parece de groselha e tem sabor de tamarindo.',
+            opening_hours: 'das 14h às 20h',
+            open_on_weekends: true
+        }
+
+        cy.apiCreateUser(user)
+        cy.uiLogin(user)
+
+        mapPage.createLink()
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('Food truck cadastrado com sucesso!')
+      
+    });
+    
+});
