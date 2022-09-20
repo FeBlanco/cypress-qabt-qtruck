9e03dab576798066990f7b629487c3aa8ee8fe93
Felipe Blanco <feeh.blanco@gmail.com>
2022-09-19 22:45:10 -0300

    tests: adicionando o tesults reports

---
+++ tests/runner.js
@@ -0,0 +1,16 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter');
+
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImUwNzM5ZDYyLWYxZDktNDgxMi1hMWE4LTU3NDVmMTg5M2E3ZC0xNjYzNjM2NzAxNTI3IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZWRlZWJjNTctMjM0Ni00NjdiLWFlZDAtZWFhODNjZjg4YzYxIiwidHlwZSI6InQifQ.KmrCURgYbXl61uOh3jnjsEYhBxufx1S1yrhiZFzgQWk'
+cypress.run({
+  // specs to run here
+})
+.then((results) => {
+  const args = {
+    target: TOKEN,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+ console.error(err)
+})
